3b87e3c9d2bbe9ebddd217879df0b10d3ad8ccb9
ethan <ethanm4984@gmail.com>
2020-07-22 21:50:03 -0600

    isr

--- lib/asmUtils.h
+++ lib/asmUtils.h
@@ -1,12 +1,36 @@
 #ifndef ASMUTILS_H
 #define ASMUTILS_H
 
-#include <stdint.h>
-
 #define COM1 0x3f8 
 #define COM2 0x2f8
 #define COM3 0x3e8
 #define COM4 0x2e8
+
+#include <stdint.h>
+
+typedef struct {
+    uint64_t r15;
+    uint64_t r14;
+    uint64_t r13;
+    uint64_t r12;
+    uint64_t r11;
+    uint64_t r10;
+    uint64_t r9;
+    uint64_t r8;
+    uint64_t rsi;
+    uint64_t rdi;
+    uint64_t rbp;
+    uint64_t rdx;
+    uint64_t rcx;
+    uint64_t rbx;
+    uint64_t rax;
+    uint64_t isrNumber;
+    uint64_t rip;
+    uint64_t cs; 
+    uint64_t rflags; 
+    uint64_t rsp;
+    uint64_t ss;
+} __attribute__((packed)) regs_t;
 
 uint8_t serialRead();
 
